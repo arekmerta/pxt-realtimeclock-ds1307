c8d5ce154f9b1686ce6bd29676c97bcfc123bef4
arek <arek.dot.merta@gmail.com>
2018-01-27 19:33:32 +0100

    getCurrent time as export

--- shims.d.ts
+++ shims.d.ts
@@ -4,21 +4,16 @@
 
     //% color=80 weight=80
     //% icon="\uf017"
-declare namespace Time {
+ namespace Time {
 
     /**
      * gets the current time.
      */
     //% blockId=rtc_ds_get_time
     //% block="the current time" shim=Time::getCurrentTime
-    function getCurrentTime(): string{
-   basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+    export function getCurrentTime(): string {
+
+        return "z:z"    
     }
 
     /**
@@ -26,28 +21,28 @@ declare namespace Time {
      */
     //% blockId=rtc_ds_get_unixtime
     //% block="the current unix timestamp" shim=Time::getUnixTimestamp
-    function getUnixTimestamp(): number;
+    export function getUnixTimestamp(): number;
 
     /**
      * gets the current datetimepart.
      */
     //% blockId=rtc_ds_get_part
     //% block="the current %part" shim=Time::getTimePart
-    function getTimePart(part: DateTimePart): number;
+    export function getTimePart(part: DateTimePart): number;
 
     /**
      * sets the current time.
      */
     //% blockId=rtc_ds_set_unixtime
     //% block="set unix time to |%uxtime" shim=Time::setUnixTime
-    function setUnixTime(uxtime: number): void;
+    export function setUnixTime(uxtime: number): void;
 
     /**
      * sets the current datetimepart.
      */
     //% blockId=rtc_ds_set_part
     //% block="set the current %part| to |%val" shim=Time::setTimePart
-    function setTimePart(part: DateTimePart, val: number): void;
+    export function setTimePart(part: DateTimePart, val: number): void;
 }
 
 // Auto-generated. Do not edit. Really.
