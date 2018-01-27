b5c39d70028966676273b520c36789a904eec79f
arek <arek.dot.merta@gmail.com>
2018-01-27 21:12:37 +0100

    change block names

--- main.ts
+++ main.ts
@@ -6,10 +6,20 @@
     //% icon="\uf017"
  namespace Time {
 
+var DateTimePart = {
+    Seconds : 0,
+    Minutes : 1,
+    Hours : 2,
+    Day : 3,
+    Month : 4,
+    Year : 5,
+};
+
+
     /**
      * gets the current time.
      */
-    //% weight=30 blockId="rtc_ds_get_time" block="Time"
+    //% weight=30 blockId="rtc_ds_get_time" block="Get Current Time"
     export function getCurrentTime(): string {
 
         return "z:z"    
@@ -18,7 +28,7 @@
     /**
      * gets the current unix timestamp.
      */
-    //% weight=30 blockId="rtc_ds_get_unixtime" block="Time"
+    //% weight=30 blockId="rtc_ds_get_unixtime" block="Get Unix Timestamp"
     export function getUnixTimestamp(): number{
 return 7
 }
@@ -26,7 +36,7 @@ return 7
     /**
      * gets the current datetimepart.
      */
-    //% weight=30 blockId="rtc_ds_get_part" block="Time"
+    //% weight=30 blockId="rtc_ds_get_part" block="Get Time Part"
     export function getTimePart(part: DateTimePart): number{
     return 6;
 }
@@ -34,14 +44,14 @@ return 7
     /**
      * sets the current time.
      */
-    //% weight=30 blockId="rtc_ds_set_unixtime" block="Time"
+    //% weight=30 blockId="rtc_ds_set_unixtime" block="Set Unix Time"
     export function setUnixTime(uxtime: number): void{
 }
 
     /**
      * sets the current datetimepart.
      */
-    //% weight=30 blockId="rtc_ds_set_part" block="Time"
+    //% weight=30 blockId="rtc_ds_set_part" block="Set Time Part"
     export function setTimePart(part: DateTimePart, val: number): void{
     }
 }
