15246b13e44a102b47d947c90b4418095675d2ce
arek <arek.dot.merta@gmail.com>
2018-01-27 19:38:12 +0100

    adding main to package

--- main.ts
+++ main.ts
@@ -1,7 +1,46 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+// Auto-generated. Do not edit.
+
+
+
+    //% color=80 weight=80
+    //% icon="\uf017"
+ namespace Time {
+
+    /**
+     * gets the current time.
+     */
+    //% blockId=rtc_ds_get_time
+    //% block="the current time" shim=Time::getCurrentTime
+    export function getCurrentTime(): string {
+
+        return "z:z"    
+    }
+
+    /**
+     * gets the current unix timestamp.
+     */
+    //% blockId=rtc_ds_get_unixtime
+    //% block="the current unix timestamp" shim=Time::getUnixTimestamp
+    export function getUnixTimestamp(): number;
+
+    /**
+     * gets the current datetimepart.
+     */
+    //% blockId=rtc_ds_get_part
+    //% block="the current %part" shim=Time::getTimePart
+    export function getTimePart(part: DateTimePart): number;
+
+    /**
+     * sets the current time.
+     */
+    //% blockId=rtc_ds_set_unixtime
+    //% block="set unix time to |%uxtime" shim=Time::setUnixTime
+    export function setUnixTime(uxtime: number): void;
+
+    /**
+     * sets the current datetimepart.
+     */
+    //% blockId=rtc_ds_set_part
+    //% block="set the current %part| to |%val" shim=Time::setTimePart
+    export function setTimePart(part: DateTimePart, val: number): void;
+}
--- shims.d.ts
+++ shims.d.ts
@@ -11,38 +11,35 @@
      */
     //% blockId=rtc_ds_get_time
     //% block="the current time" shim=Time::getCurrentTime
-    export function getCurrentTime(): string {
-
-        return "z:z"    
-    }
+     function getCurrentTime(): string ;
 
     /**
      * gets the current unix timestamp.
      */
     //% blockId=rtc_ds_get_unixtime
     //% block="the current unix timestamp" shim=Time::getUnixTimestamp
-    export function getUnixTimestamp(): number;
+     function getUnixTimestamp(): number;
 
     /**
      * gets the current datetimepart.
      */
     //% blockId=rtc_ds_get_part
     //% block="the current %part" shim=Time::getTimePart
-    export function getTimePart(part: DateTimePart): number;
+     function getTimePart(part: DateTimePart): number;
 
     /**
      * sets the current time.
      */
     //% blockId=rtc_ds_set_unixtime
     //% block="set unix time to |%uxtime" shim=Time::setUnixTime
-    export function setUnixTime(uxtime: number): void;
+     function setUnixTime(uxtime: number): void;
 
     /**
      * sets the current datetimepart.
      */
     //% blockId=rtc_ds_set_part
     //% block="set the current %part| to |%val" shim=Time::setTimePart
-    export function setTimePart(part: DateTimePart, val: number): void;
+     function setTimePart(part: DateTimePart, val: number): void;
 }
 
 // Auto-generated. Do not edit. Really.
