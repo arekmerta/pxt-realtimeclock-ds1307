8cfa6c1d127ee9cebdd7a7bb0a9e3a073c4d78ea
arek <arek.dot.merta@gmail.com>
2018-01-27 16:41:35 +0100

    Added export to shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -11,35 +11,35 @@ declare namespace Time {
      */
     //% blockId=rtc_ds_get_time
     //% block="the current time" shim=Time::getCurrentTime
-    function getCurrentTime(): string;
+    export function getCurrentTime(): string;
 
     /**
      * gets the current unix timestamp.
      */
     //% blockId=rtc_ds_get_unixtime
     //% block="the current unix timestamp" shim=Time::getUnixTimestamp
-    function getUnixTimestamp(): number;
+    export function getUnixTimestamp(): number;
 
     /**
      * gets the current datetimepart.
      */
     //% blockId=rtc_ds_get_part
     //% block="the current %part" shim=Time::getTimePart
-    function getTimePart(part: DateTimePart): number;
+    export function getTimePart(part: DateTimePart): number;
 
     /**
      * sets the current time.
      */
     //% blockId=rtc_ds_set_unixtime
     //% block="set unix time to |%uxtime" shim=Time::setUnixTime
-    function setUnixTime(uxtime: number): void;
+    export function setUnixTime(uxtime: number): void;
 
     /**
      * sets the current datetimepart.
      */
     //% blockId=rtc_ds_set_part
     //% block="set the current %part| to |%val" shim=Time::setTimePart
-    function setTimePart(part: DateTimePart, val: number): void;
+    export function setTimePart(part: DateTimePart, val: number): void;
 }
 
 // Auto-generated. Do not edit. Really.
